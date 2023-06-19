9f47035d69be4e57a391f04222cec259b141bebb
Neatwhiskeyy <neatwhiskey24@gmail.com>
2023-06-19 16:49:48 +0100

    Initial Commit

---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Jamaaldeen Opasina on 19/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
